7a62478ad5b8ab83b769422e6bcbe1875c7f5d87
ArHiFranki <franki550@gmail.com>
2021-11-23 12:24:11 +0700

    Добавил анимации Idle и Walk, аниматор и управляющий скрипт

--- Assets/Scripts/PlatformerPlayer.cs
+++ Assets/Scripts/PlatformerPlayer.cs
@@ -2,16 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(Animator))]
 public class PlatformerPlayer : MonoBehaviour
 {
     [SerializeField] private float _speed = 250.0f;
 
     private Rigidbody2D _rigidbody;
+    private Animator _animator;
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -19,5 +21,11 @@ public class PlatformerPlayer : MonoBehaviour
         float deltaX = Input.GetAxis("Horizontal") * _speed * Time.deltaTime;
         Vector2 movement = new Vector2(deltaX, _rigidbody.velocity.y);
         _rigidbody.velocity = movement;
+
+        _animator.SetFloat("speed", Mathf.Abs(deltaX));
+        if (!Mathf.Approximately(deltaX, 0))
+        {
+            transform.localScale = new Vector3(Mathf.Sign(deltaX), 1, 1);
+        }
     }
 }
